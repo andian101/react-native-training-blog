827184f4fcfe4466bfb35e207148d646746f3c8b
Ian <ian.moss@and.digital>
2021-02-13 11:02:11 +0000

    Added memoisation

--- src/context/actions.js
+++ src/context/actions.js
@@ -1,12 +1,13 @@
+import {useCallback} from 'react';
 import {SET_POSTS} from './reducer';
 
 export const useActions = (dispatch) => {
-  const setPosts = (posts) => {
+  const setPosts = useCallback((posts) => {
     dispatch({
       type: SET_POSTS,
       posts,
     });
-  };
+  });
 
   return {
     setPosts,
