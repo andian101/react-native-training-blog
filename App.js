a9b5abafcc8e07a69c2ae84735b49a9a5c717bcb
Jemal Ahmedov <jemaljahmedov@gmail.com>
2021-02-13 11:28:22 +0000

    Fix lintin

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import {SafeAreaView} from 'react-native';
 import Listing from './src/screens/Listing';
 
 const App = () => {
--- src/screens/Listing/index.js
+++ src/screens/Listing/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, Image, FlatList, ScrollView} from 'react-native';
+import {View, Text, Image, FlatList} from 'react-native';
 import images from '../../assets/images';
 
 // 1. Build component
@@ -8,14 +8,13 @@ import images from '../../assets/images';
 // 4. Render the data onto the screen
 // 5. Add loading spinner
 
-
 // {"content": "We need to bypass the auxiliary EXE pixel!", "createdAt": "2021-02-03T21:26:28.632Z", "featuredImage": "http://lorempixel.com/640/480/animals", "id": "1", "likes": [{"blogId": "1", "createdAt": 1612431246, "id": "1", "userId": 17}], "tags": [], "title": "District Communications Planner", "userId": 99}
 
 const BlogListElement = ({title, featuredImage, content}) => {
   return (
     <View>
       <Image
-        source={{ uri: featuredImage }}
+        source={{uri: featuredImage}}
         style={{
           width: 50,
           height: 50,
@@ -32,29 +31,34 @@ const Listing = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const posts = await (await fetch("https://5f843a3c6b97440016f4f2dc.mockapi.io/blogs")).json();
+      const posts = await (
+        await fetch('https://5f843a3c6b97440016f4f2dc.mockapi.io/blogs')
+      ).json();
       setTimeout(() => {
-        setPosts(posts.items.map(item => {
-          item.featuredImage = 'https://placeimg.com/640/480/any';
-          return item;
-        }));
-      }, 1000)
-    }
+        setPosts(
+          posts.items.map((item) => {
+            item.featuredImage = 'https://placeimg.com/640/480/any';
+            return item;
+          }),
+        );
+      }, 1000);
+    };
 
     fetchData();
   }, []);
 
   return (
     <View>
-      { !posts.length && <Image source={images.spinner} style={{width: 100, height: 100}} /> }
-      {
-        !!posts.length && 
-        <FlatList 
+      {!posts.length && (
+        <Image source={images.spinner} style={{width: 100, height: 100}} />
+      )}
+      {!!posts.length && (
+        <FlatList
           data={posts}
           renderItem={({item}) => <BlogListElement {...item} />}
-          keyExtractor={(item) =>item.id}
+          keyExtractor={(item) => item.id}
         />
-      }
+      )}
     </View>
   );
 };
