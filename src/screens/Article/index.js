69b7d1edb2b0c5b1028eeda10335e5eb24e972dc
Jemal Ahmedov <jemaljahmedov@gmail.com>
2021-02-16 17:56:18 +0000

    Move styles to separate file

--- src/screens/Article/index.js
+++ src/screens/Article/index.js
@@ -1,8 +1,9 @@
 import {useNavigation} from '@react-navigation/native';
 import React from 'react';
 import {useEffect} from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
 import Tag from '../../components/Tag';
+import styles from './styles';
 
 // 1. Get data from props
 // 2. Build component
@@ -60,37 +61,5 @@ const ArticleScreen = (props) => {
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  image: {
-    width: '100%',
-    height: 320,
-    marginBottom: 10,
-  },
-  container: {
-    marginHorizontal: 15,
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: 'bold',
-  },
-  content: {
-    marginTop: 30,
-    fontSize: 20,
-  },
-  tagsWrapper: {
-    marginTop: 16,
-    flexDirection: 'row',
-  },
-  articleDetail: {
-    color: '#64707d',
-    fontSize: 17,
-  },
-  articleDetailsWrapper: {
-    marginTop: 16,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-});
 
 export default ArticleScreen;
---
+++ src/screens/Article/styles.js
@@ -0,0 +1,33 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 320,
+    marginBottom: 10,
+  },
+  container: {
+    marginHorizontal: 15,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  content: {
+    marginTop: 30,
+    fontSize: 20,
+  },
+  tagsWrapper: {
+    marginTop: 16,
+    flexDirection: 'row',
+  },
+  articleDetail: {
+    color: '#64707d',
+    fontSize: 17,
+  },
+  articleDetailsWrapper: {
+    marginTop: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
