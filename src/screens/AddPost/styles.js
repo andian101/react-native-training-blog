f43bbac2a1bb4da7ebd8f30230c86975b0244603
Jemal Ahmedov <jemaljahmedov@gmail.com>
2021-02-16 17:58:57 +0000

    Add Post styles in separate file

---
+++ src/screens/AddPost/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    margin: 20,
+  },
+  screenTitle: {
+    fontSize: 30,
+    marginBottom: 20,
+  },
+  inputFieldBase: {
+    fontSize: 18,
+    marginTop: 20,
+    padding: 10,
+  },
+  inputField: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+  },
+  actionBtn: {
+    marginHorizontal: 20,
+  },
+  actionBtnLabel: {
+    fontSize: 18,
+    color: '#3786de',
+  },
+  errorMsg: {
+    fontSize: 16,
+    color: '#c20a0a',
+  },
+});
