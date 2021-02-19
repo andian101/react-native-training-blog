f88b950347028d4aaa372720998d7a42abed7d6e
Jemal Ahmedov <jemaljahmedov@gmail.com>
2021-02-19 09:33:35 +0000

    Align screen title

--- src/navigation/routes.js
+++ src/navigation/routes.js
@@ -4,18 +4,22 @@ import {screenTypes} from './constants';
 import Listing from '../screens/Listing';
 import Article from '../screens/Article';
 
+const sharedOptions = {
+  headerTitleAlign: 'center',
+};
+
 const AppStack = createStackNavigator();
 
 const Routes = () => {
   return (
     <AppStack.Navigator>
       <AppStack.Screen
-        options={{title: 'Blog'}}
+        options={{title: 'Blog', ...sharedOptions}}
         name={screenTypes.listing}
         component={Listing}
       />
       <AppStack.Screen
-        options={{title: 'Articles'}}
+        options={{title: 'Articles', ...sharedOptions}}
         name={screenTypes.article}
         component={Article}
       />
